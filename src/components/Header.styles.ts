fc3683b43568fda8f3ba808040af4d300fde87d8
ocampco <ocampco@gmail.com>
2025-06-07 14:35:12 +1000

    chore: use emoji instead of logo

--- src/components/Header.styles.ts
+++ src/components/Header.styles.ts
@@ -6,11 +6,6 @@ export const Container = styled.div`
   margin-bottom: 2rem;
 `;
 
-export const Logo = styled.img`
-  height: 4rem;
-  margin-right: 1rem;
-`;
-
 export const Heading = styled.div`
   font-size: 1.5rem;
   font-weight: 700;
